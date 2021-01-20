1ae7b0653f0f678ad798d102c3afd1d0097879c5
jyotinayal <48470518+jyotinayal@users.noreply.github.com>
2021-01-20 11:50:28 +0530

    Update Calculator.c

--- Calculator.c
+++ Calculator.c
@@ -3,11 +3,11 @@
 int main()
 {
   //changed variable naemes.....................
-   int number_one=0;
-   int number_two=0;
+   int a=0;
+   int b=0;
 
     printf("Enter two Numbers: ");
-    scanf("%d %d", &number_one, &number_two);
-    printf("%d",  number_one + number_two);
+    scanf("%d %d", &a, &b);
+    printf("%d",  a + b);
     return 0;
 }
