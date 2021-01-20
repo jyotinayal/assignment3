c24171cfa4a34e1687d44a75bbfde534b39fc17c
Gaurav Pingale <gauravpingale4@gmail.com>
2021-01-20 11:46:20 +0530

    variable name shortend

--- Calculator.c
+++ Calculator.c
@@ -3,11 +3,12 @@
 int main()
 {
   //changed variable naemes.....................
-   int number_one=0;
-   int number_two=0;
+   int number1=0;
+   int number1=0;
 
     printf("Enter two Numbers: ");
-    scanf("%d %d", &number_one, &number_two);
-    printf("%d",  number_one + number_two);
+    scanf("%d %d", &number1, &numbe2);
+    printf("%s",  "Result=");
+    printf("%d",  number1 + number2);
     return 0;
 }
