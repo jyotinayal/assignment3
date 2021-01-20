d21ac2fe96bfc590cc14eb3f6b9ec9387919a75c
Sooraj Gaikwad <suraj.gaikwad@cuelogic.com>
2021-01-20 11:37:23 +0530

    adding comments

--- Calculator.c
+++ Calculator.c
@@ -2,12 +2,12 @@
 
 int main()
 {
-  //changed variable naemes.....................
-   int number_one=0;
-   int number_two=0;
+  //changed variable naemes.................................
+   int number_one=0; //
+   int number_two=0; // 
 
-    printf("Enter two Numbers: ");
-    scanf("%d %d", &number_one, &number_two);
-    printf("%d",  number_one + number_two);
+    printf("Enter two Numbers: "); //
+    scanf("%d %d", &number_one, &number_two);//
+    printf("%d",  number_one + number_two);//
     return 0;
 }
