62e608d26af36df293538209846cfe1db0d69ea9
jyoti nayal <jyoti.nayal@cuelogic.com>
2021-01-20 09:59:21 +0530

    Calculator file added

---
+++ Calculator.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+   int first,second;
+   printf("Enter two operands: ");
+    scanf("%d %d", &first, &second);
+    printf("%d",  first + second);
+    return 0;
+}
